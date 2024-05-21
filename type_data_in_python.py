c7855829c9157ad0b72673bd3f77be80e17d0419
whyNOT <whyN0t7@proton.me>
2024-05-21 09:51:01 +0300

    edit first

--- type_data_in_python.py
+++ type_data_in_python.py
@@ -1,20 +1,17 @@
 # 1. Напишіть програму, яка зчитує два цілих числа A та B і виводить найбільше значення з них.
 # 1. Write a program that reads two integers A and B and outputs the largest value from them.
 
-first_value: int = int(input("Write your first number here: "))
-second_value: int = int(input("Write your second number here: "))
+def largest_value(first_value: int, second_value: int) -> None:
+    if int(first_value) > int(second_value):
+        return print(f"Your first number {first_value} is greater than your second number {second_value}")
+    elif int(second_value) > int(first_value):
+        return print(f"Your second number {second_value} is greater than your first number {first_value}")
 
-first_answer = f"Your first number {first_value} is greater than your second number {second_value}"
-second_answer = f"Your second number {second_value} is greater than your first number {first_value}"
 
-if first_value > second_value:
-    print(first_answer)
-elif second_value > first_value:
-    print(second_answer)
+first_type: int = int(input("Write your first number here: "))
+second_type: int = int(input("Write your second number here: "))
+
+largest_value(first_type, second_type)
 
 # 2. Написати програму визначення координатної чверті, перевіряючи спочатку координату «Y».
 # 2. Write a program to determine the coordinate quarter, first checking the "Y" coordinate.
-
-def test_fink():
-
-    pass
