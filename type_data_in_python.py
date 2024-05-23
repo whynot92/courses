0c14ba68e07e120a7dbcdc07000a083f7c3fc3d1
whynot92 <why_n0t7@proton.me>
2024-05-23 10:39:12 +0000

    added the English translation of the tasks

--- type_data_in_python.py
+++ type_data_in_python.py
@@ -1,4 +1,6 @@
 # 1. Створити дві змінні типу string і помістити туди ім’я та прізвище. Вивести привітання, враховуючи персональні дані:
+# 1, Create two string variables and put the first and last name there. Display greetings, taking into account personal
+# data:
 
 first_name: str = 'Vadym'
 second_name: str = 'Sokolov'
@@ -6,6 +8,8 @@ second_name: str = 'Sokolov'
 print(f'Hello {first_name} {second_name}!!!')
 
 # 2. Ввести з клавіатури 2 числа, знайти різницю, суму, добуток та остачу першого числа від другого:
+# 2. Enter 2 numbers from the keyboard, find the difference, sum, product and remainder of the first number
+# from the second:
 
 first_input_user: int = int(input('Write here your first number: '))
 second_input_user: int = int(input('Write here your second number: '))
@@ -25,6 +29,7 @@ print(first_input_user ** second_input_user)
 print(first_input_user % second_input_user)
 
 # 3. Знайдіть результат операції:
+# 3. Find the result of the operation:
 
 print(f'10 + 10 // 2 = {10 + 10 // 2}')    # 15
 print(f'18 / 3 ** 2 = {18 / 3 ** 2}')    # 2.0
@@ -38,6 +43,7 @@ print(f'16 // 4 ** 2 = {16 // 4 ** 2}')    # 1
 print(f'30 - 15 * 2 = {30 - 15 * 2}')    # 0
 
 # 4. Знайдіть результат операції:
+# 4. Find the result of the operation:
 
 print(f'7 * 3 <= 25 - {7 * 3 <= 25}')   # True
 print(f'12 % 5 >= 2 - {12 % 5 >= 2}')   # True
@@ -57,6 +63,8 @@ print(f'(10 / 2) + (8 - 4) > 9 - {(10 / 2) + (8 - 4) > 9}')    # False
 
 # 5. Напишіть програму, яка за даним N від 1 до 9 виводить на екран N пінгвінів. Зображення одного пінгвіна має розмір
 # 5×9 символів, між двома сусідніми пінгвінами також є порожній (з прогалин) стовпець.
+# 5. Write a program that, given N from 1 to 9, displays N penguins on the screen. The image of one penguin has the size
+# 5×9 symbols, there is also an empty (from spaces) column between two adjacent penguins.
 
 # number: int = int(input("Write your number from 1 - 9: "))
 #
@@ -71,13 +79,17 @@ print(f'(10 / 2) + (8 - 4) > 9 - {(10 / 2) + (8 - 4) > 9}')    # False
 
 # 6. N школярів ділять K яблук порівну, залишок, що не ділиться, залишається в кошику. Скільки яблук дістанеться
 # кожному школяру?
+# 6. N schoolchildren divide K apples equally, the remainder that is not divided remains in the basket.
+# How many apples will you get? to every student?
 
 number_of_students: int = int(input("Write number of students here: "))
 number_of_apples: int = int(input("Write number of apples here: "))
 
 print(f'Each student receives {number_of_apples // number_of_students}')
 
-# 7. школярів поділили K яблук порівну, залишок, що не ділиться, залишився в кошику. Скільки яблук залишилось у кошику?
+# 7.N школярів поділили K яблук порівну, залишок, що не ділиться, залишився в кошику. Скільки яблук залишилось у кошику?
+# 7. N schoolchildren divided K apples equally, the undivided remainder remained in the basket.
+# How many apples are left in the basket?
 
 number_of_students_one: int = int(input("Write number of students here: "))
 number_of_apples_one: int = int(input("Write number of apples here: "))
@@ -85,12 +97,15 @@ number_of_apples_one: int = int(input("Write number of apples here: "))
 print(f'Left in the cart {number_of_apples_one % number_of_students_one}')
 
 # 8. Оголосити змінну - довжину сторони квадрату. Обчислити площу квадрату і вивести її на екран.
+# 8. Declare a variable - the length of the side of the square. Calculate the area of the square and
+# display it on the screen.
 
 side_of_a_square: int = 15
 
 print(f'The area of the square is {side_of_a_square * 4}')
 
 # 9. Дано натуральне число. Виведіть останню цифру.
+# 9. A natural number is given. Print the last digit.
 
 value_number: str = input("Write your number here: ")
 
@@ -98,6 +113,7 @@ gen_list = [el for el in value_number if el.isdigit()]
 print(gen_list[-1])
 
 # 10. Дано позитивне двозначне число. Знайдіть число десятків у ньому.
+# 10. A positive two-digit number is given. Find the number of tens in it.
 
 two_digit_number: int = int(input("Write a positive two-digit number here: "))
 
@@ -106,10 +122,12 @@ answer = two_digit_number // 10
 print(f'The number has {answer} tens')
 
 # 11. Запишіть літеру 'A' (латинську, велику) 100 разів поспіль.
+# 11. Write the letter 'A' (Latin, capital) 100 times in a row.
 
 print('A' * 100)
 
 # 12. Дано ціле число N. Виведіть наступне за ним парне число.
+# 12. Given an integer N. Print the next even number after it.
 
 N: int = int(input("Enter an integer: "))
 
@@ -121,6 +139,7 @@ else:
 print(f"Next for, {N}, even number:, {next_even}")
 
 # 13. Задане число N записали 100 разів поспіль і потім піднести до квадрату. Що вийшло?
+# 13. The specified number N was written down 100 times in a row and then squared. What happened?
 
 N: str = input("Write your number here: ")
 
@@ -129,12 +148,15 @@ print(f'Your answer = {int((N * 100)) ** 2}')
 
 # 14. Довжина Київської кільцевої автомобільної дороги – 107 кілометрів. Байкер Вася стартує з нульового кілометра цієї
 # дороги та їде зі швидкістю v кілометрів на годину. На якій відмітці він зупиниться через годину?
+# 14. The length of the Kyiv ring road is 107 kilometers. Biker Vasya starts from the zero kilometer of this one
+# of the road and travels at a speed of v kilometers per hour. At what mark will it stop after an hour?
 
 speed_bike: int = int(input("Write here your speed: "))
 start: int = 0
 print(f'The biker will stop at a distance of {start + speed_bike} km in one hour.')
 
 # 15. За день машина проїжджає N км. Скільки днів потрібно, щоби проїхати маршрут довжиною M кілометрів?
+# 15. A car travels N km per day. How many days does it take to drive a route M kilometers long?
 
 N: int = int(input("Enter the number of kilometers the car drives per day: "))
 M: int = int(input("Enter the total length of the route in kilometers: "))
