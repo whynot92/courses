7aaa9e730f23f14f0a4c7328c18f1e72fcc536e3
whynot92 <why_n0t7@proton.me>
2024-05-23 05:37:29 +0000

    add new homework and add 1

---
+++ control_structures.py
@@ -0,0 +1,12 @@
+
+# 1. Напишіть програму, яка зчитує два цілих числа A та B і виводить найбільше значення з них.
+
+A: int = int(input("Write first integer number here: "))
+B: int = int(input("Write second integer number here: "))
+
+if A > B:
+    print(A)
+elif A < B:
+    print(B)
+
+# 2. Написати програму визначення координатної чверті, перевіряючи спочатку координату «Y»
