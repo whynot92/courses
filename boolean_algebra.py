a9898f94538c6d01630129d492e3857fc47ce94e
whynot92 <why_n0t7@proton.me>
2024-05-24 09:17:52 +0000

    edit

---
+++ boolean_algebra.py
@@ -0,0 +1,27 @@
+#  1. Написати відповідь True або False:
+# not (False or False) =
+# False or not False =
+# not (True or False) =
+# False and not False =
+# not (False and True) =
+# True and not False =
+# not (True and False) =
+# False and not True =
+# not (False or True) =
+# True or not False =
+# not (True or True) =
+# False or not True =
+# not (True and True) =
+# True and not True =
+# not (False and False) =
+# True or not True =
+# not (True or False) =
+# False and not False =
+# not (False or True) =
+# True and not False =
+#  2. Задано число, визначити чи воно входить в діапазон від 10 до 30
+#  3. Напишіть програму, яка перевіряє, чи є число парне і додатне.
+#  4. Напишіть програму, яка перевіряє, чи є число додатним, парним та не кратним 3.
+#  5. Напишіть програму, яка перевіряє, чи є число кратним 3 або 5, але не кратним обом одночасно.
+#  6. Напишіть програму, яка перевіряє, чи є число менше за 100 і більше за 50.
+#  7. Напишіть програму, яка перевіряє, чи є число додатним і менше за 50 або від'ємним і більше за -10.
