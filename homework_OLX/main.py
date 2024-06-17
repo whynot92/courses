2fe3e5278eeb459aa07f57bf262892e67790521f
WhyNOT <whyn0t7@proton.me>
2024-06-17 21:52:23 +0300

    add files

--- homework_OLX/main.py
+++ homework_OLX/main.py
@@ -1,40 +1,60 @@
+import customtkinter as tk
 import requests
 from bs4 import BeautifulSoup
 
-number_page = 1
-url = 'https://www.olx.ua/uk/list/q-thinkpad-x280/?page={number_page}'
+def generate():
+    name_file = entry_user.get()
+    page_number = 1
+    number_pages = list()
 
-resp = requests.get(url)
-soup = BeautifulSoup(resp.text, 'lxml')
+    url = f'https://webscraper.io/test-sites/e-commerce/static/computers/laptops?page={page_number}'
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, 'lxml')
 
-links = soup.find_all('div', class_='css-qfzx1y')
+    action = soup.find_all('a', class_='title')
+    price = soup.find_all('h4', class_='price float-end card-title pull-right')
+    pages = soup.find('ul', class_='pagination')
 
+    for i in pages.text.split('\n'):
+        if i.isalnum():
+            number_pages.append(i)
+    with open(f'homework_OLX\\{name_file}.txt', 'w', encoding='utf-16') as file:
+        while page_number < int(number_pages[-1]) + 1:
+            file.write('\n')
+            file.write('-' * 40)
+            file.write(f'Страница № {page_number}')
+            file.write('-' * 40)
+            for a, p in zip(action, price):
+                links = a.get('href')
+                file.write('\n')
+                file.write(f'Ноутбук: {a.text} \n')
+                file.write(f'Цена: {p.text} \n')
+                file.write(f'https://webscraper.io/{links} \n')
+            page_number += 1
+    
+        else:
+            file.write('-' * 40)
+            file.write('End price')
+            file.write('-' * 40)
 
-if resp.status_code == 200:
-    print('\n')
-    print('-' * 70)
-    print(f'Страница {number_page}')
-    print('-' * 70)
-
-    for i in links:
+    print('Данные успешно сохранены в файл price.txt')
  
-        link = i.find_all('a', class_='css-z3gu2d')
-        prace = i.find_all('p', class_='css-tyui9s er34gjf0')
-        name = i.find_all('h6', class_='css-16v5mdi er34gjf0')
-        for a, b, c in zip(name, link, prace):
-            article_link = b.get('href')
-            print('\n')
-            print(a.text)
-            print(f'Цена: {c.text}')
-            print(f'Ссылка: https://olx.ua{article_link}')
 
-    print('\n')
-    print('-' * 70)
-    print(f'Страница {number_page + 1}')
-    print('-' * 70)
-    print('\n')
+tk.set_appearance_mode('System')
+
+window = tk.CTk()
+window.title('Scraper site')
+window.iconbitmap('homework_OLX\\seo_marketing_finance_management_business_icon_263053.ico')
+window.geometry('200x200')
 
 
-else:
-    print(f"Не удалось получить доступ к странице")
+bg = tk.CTkLabel(window, text='Write here name your file:')
+bg.pack()
 
+entry_user = tk.CTkEntry(window)
+entry_user.pack()
+
+bt_generate = tk.CTkButton(window, text='Generat prices', command=generate)
+bt_generate.place(x=30, y=80)
+
+window.mainloop()
